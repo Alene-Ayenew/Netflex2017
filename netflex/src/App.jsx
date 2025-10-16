a7cd3cdf8db3abf07fa63e37d687858589eef895
Alene-Ayenew <aleneayenew14@gmail.com>
2025-10-16 14:41:38 +0300

    my fist project

---
+++ netflex/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <div className='App'>
+         <h1>React</h1>
+      </div>
+     
+      
+    </>
+  )
+}
+
+export default App

61db7181291b72964411ac4bba4cd9af21d017d3
Alene-Ayenew <aleneayenew14@gmail.com>
2025-10-16 20:49:55 +0300

    header and footer of netflix

--- netflex/src/App.jsx
+++ netflex/src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
+import './App.css'
+import Header from './Components/Header/Header.jsx'
 
-
+import Footer from './Components/Footer/Footer'
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       
       <div className='App'>
-         <h1>React</h1>
+        <Header/>
+        <Footer/>
       </div>
      
       
